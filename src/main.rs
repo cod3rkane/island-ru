9ec21be605e9838d904233078093920e1dc7aa59
Cod3rkane <jcesar.developer@gmail.com>
2020-07-10 22:46:13 -0400

    adding GL

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,41 @@ extern crate glfw;
 
 use glfw::{ Action, Context, Key };
 
+extern crate gl;
+
 fn main() {
     println!("Hello, world!");
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    let (mut window, events) = glfw.create_window(1280, 720, "Island Something - Cod3r Kane", glfw::WindowMode::Windowed).expect("Failed to Create GLFW Window");
+    
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
-    window.set_key_polling(true);
+    let (mut window, events) = glfw.create_window(1280, 720, "Island Something - Cod3r Kane", glfw::WindowMode::Windowed)
+        .expect("Failed to Create GLFW Window");
+
     window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
     while !window.should_close() {
-        glfw.poll_events();
-
         for (_, events) in glfw::flush_messages(&events) {
             handle_window_events(&mut window, events);
         }
+
+        window.swap_buffers();
+        glfw.poll_events();
     }
 }
 
 fn handle_window_events(window: &mut glfw::Window, event: glfw::WindowEvent) {
     match event {
+        glfw::WindowEvent::FramebufferSize(width, height) => {
+            unsafe { gl::Viewport(0, 0, width, height) }
+        }
         glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
             window.set_should_close(true);
         }
