fbfcb2402de1c8e3d4d97339c6a1ee4ad234f0ee
Cod3rkane <jcesar.developer@gmail.com>
2020-07-10 22:20:28 -0400

    Adding GLFW and Hello First Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate glfw;
+
+use glfw::{ Action, Context, Key };
+
 fn main() {
     println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, events) = glfw.create_window(1280, 720, "Island Something - Cod3r Kane", glfw::WindowMode::Windowed).expect("Failed to Create GLFW Window");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+
+        for (_, events) in glfw::flush_messages(&events) {
+            handle_window_events(&mut window, events);
+        }
+    }
+}
+
+fn handle_window_events(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true);
+        }
+        _ => {}
+    }
 }
