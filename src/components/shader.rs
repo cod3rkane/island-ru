72604af1396fd52fb84562a0dc39be4c8a637289
Cod3rkane <jcesar.developer@gmail.com>
2020-07-15 11:45:14 -0400

    Comment Shader::LocationIndex

--- src/components/shader.rs
+++ src/components/shader.rs
@@ -1,12 +1,12 @@
 use gl::types::{ GLuint };
 
-pub enum LocationIndex {
-    VertexPosition = 0,
-    VertexColor = 1,
-    MatrixProjection = 2,
-    MatrixView = 3,
-    MatrixModel = 4,
-}
+// pub enum LocationIndex {
+//     VertexPosition = 0,
+//     VertexColor = 1,
+//     MatrixProjection = 2,
+//     MatrixView = 3,
+//     MatrixModel = 4,
+// }
 
 pub struct Shader {
     pub program_id: GLuint,
