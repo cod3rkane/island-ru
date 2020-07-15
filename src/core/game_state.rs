9c4ccc2e94f6538b153867515d2a371fe094e401
Cod3rkane <jcesar.developer@gmail.com>
2020-07-15 07:22:32 -0400

    new GameState

--- src/core/game_state.rs
+++ src/core/game_state.rs
@@ -1,11 +1,16 @@
 use crate::components::entity::Entity;
+use crate::core::buffers::*;
 
 pub struct GameState {
     pub entities: Vec<Option<Entity>>,
+    pub buffers: Vec<Buffer>,
 }
 
 pub fn initial_game_state() -> GameState {
+    let mut initial_buffer: Buffer = Buffer::new();
+
     GameState {
         entities: vec![],
+        buffers: vec![initial_buffer],
     }
 }
