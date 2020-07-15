6e61e64b9bd1693fcd309ebe7112498e17a73992
Cod3rkane <jcesar.developer@gmail.com>
2020-07-15 08:28:28 -0400

    Adding newSquare to Entity

--- src/components/entity.rs
+++ src/components/entity.rs
@@ -2,5 +2,33 @@ use crate::components::{ physics::Physics, mesh::Mesh };
 
 pub struct Entity {
     pub physics: Option<Physics>,
-    pub mesh: Option<Vec<Mesh>>,
+    pub mesh: Mesh,
+}
+
+impl Entity {
+    pub fn newSquare() -> Entity {
+        let _triangle = Mesh {
+            vertices: vec![
+                -0.5, 0.0, 0.0,
+                0.0, 0.5, 0.0,
+                0.5, 0.0, 0.0,
+                0.0, -0.5, 0.0,
+            ],
+            indices: vec![
+                0, 1, 2,
+                0, 2, 3,
+            ],
+            colors: vec![
+                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0,
+            ],
+        };
+
+        Entity {
+            physics: None,
+            mesh: _triangle,
+        }
+    }
 }
