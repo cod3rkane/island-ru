a57939762c0eeee86631ec7edd185badf9acff0b
Cod3rkane <jcesar.developer@gmail.com>
2020-07-15 11:45:00 -0400

    GameState improvements

--- src/core/game_state.rs
+++ src/core/game_state.rs
@@ -1,16 +1,21 @@
 use crate::components::entity::Entity;
+use crate::components::shader::{ Shader };
 use crate::core::buffers::*;
+use crate::core::shader::{ create_shader };
 
 pub struct GameState {
     pub entities: Vec<Option<Entity>>,
     pub buffers: Vec<Buffer>,
+    pub current_shader: Shader,
 }
 
 pub fn initial_game_state() -> GameState {
-    let mut initial_buffer: Buffer = Buffer::new();
+    let initial_buffer: Buffer = Buffer::new();
+    let current_shader: Shader = create_shader("src/resources/vertex.glsl", "src/resources/fragment.glsl");
 
     GameState {
-        entities: vec![Some(Entity::newSquare())],
+        entities: vec![Some(Entity::new_square())],
         buffers: vec![initial_buffer],
+        current_shader,
     }
 }
