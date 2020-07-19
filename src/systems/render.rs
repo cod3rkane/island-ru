1cc30b50373c52f04dd5e5beab3f7f9f7e91d689
Cod3rkane <jcesar.developer@gmail.com>
2020-07-19 19:03:18 -0400

    improves Render System

--- src/systems/render.rs
+++ src/systems/render.rs
@@ -1,8 +1,18 @@
 use gl;
+extern crate nalgebra_glm as glm;
 
 use crate::core::game_state::GameState;
+use std::ffi::{ CStr };
+
+macro_rules! c_str {
+    ($literal:expr) => {
+        CStr::from_bytes_with_nul_unchecked(concat!($literal, "\0").as_bytes())
+    }
+}
 
 pub fn render_system(game_state: &mut GameState) {
+    // @TODO: we need to loop trough all buffers
+    // @TODO: also, we need to get all entities and create a big chunk of gathered meshes.
     game_state.buffers.get(0).unwrap().bind();
 
     game_state.buffers.get(0).unwrap().vertices_vbo.bind();
@@ -25,9 +35,35 @@ pub fn render_system(game_state: &mut GameState) {
         game_state.entities.get(0).unwrap().as_ref().unwrap().mesh.indices.as_ptr() as *const gl::types::GLvoid,
     );
 
+    let mut _transform: glm::Mat4 = glm::mat4(
+        1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0,
+    );
+    _transform = glm::translate(&mut _transform, &mut glm::vec3(0.5, -0.5, 0.0));
+    _transform = glm::scale(&mut _transform, &mut glm::vec3(0.5, 0.5, 0.5));
+
+
+    let _transform_loc: i32 =  unsafe { gl::GetUniformLocation(game_state.current_shader.program_id, c_str!("transform").as_ptr()) };
+
     unsafe {
+        gl::UniformMatrix4fv(
+            _transform_loc,
+            1,
+            gl::FALSE,
+            _transform.as_ptr(),
+        );
+
+        gl::Enable(gl::BLEND);
+        gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
         gl::UseProgram(game_state.current_shader.program_id);
         gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
+
+        gl::BindVertexArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+
+        gl::Disable(gl::BLEND);
     }
 }
 
