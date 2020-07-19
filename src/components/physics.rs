f99b61eac7d948c3fd916048d495b8653917a6a6
Cod3rkane <jcesar.developer@gmail.com>
2020-07-19 19:04:33 -0400

    Physics component

--- src/components/physics.rs
+++ src/components/physics.rs
@@ -1,5 +1,39 @@
-use cgmath::{ Vector3 };
+use nalgebra_glm::{ Vec3, Mat4, mat4, translate, rotate_z };
 
 pub struct Physics {
-    pub position: Vector3<f32>,
+    pub position: Vec3,
+    pub transform: Mat4,
+}
+
+impl Physics {
+    pub fn new(position: Vec3) -> Physics {
+        let mut matrix: Mat4 = mat4(
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        );
+        matrix = translate(&mut matrix, &position);
+
+        Physics {
+            position,
+            transform: matrix,
+        }
+    }
+
+    pub fn rotate_z(&mut self, angle: f32) {
+        self.transform = rotate_z(&mut self.transform, angle);
+    }
 }
