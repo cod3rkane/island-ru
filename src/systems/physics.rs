ee820a1ada8663ae0a0399775a8623a7c8f6bbac
Cod3rkane <jcesar.developer@gmail.com>
2020-07-19 23:13:17 -0400

    clean code

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -3,34 +3,26 @@ extern crate nalgebra_glm as glm;
 
 pub fn physics_system(game_state: &mut GameState) {
     for item in &mut game_state.entities {
-        if item.is_some() {
-            let mut index: usize = 0;
-
-            match item.as_mut() {
-                Some(x) => {
-                    loop {
-                        let mut _position: glm::Vec4 = glm::vec4(0.0, 0.0, 0.0, 0.01);
-                        if index == x.mesh.vertices.len() {
-                            break;
-                        }
-        
-                        _position = x.physics.as_ref().unwrap().transform * glm::vec4(
-                            x.mesh.vertices[index],
-                         x.mesh.vertices[index + 1],
-                         x.mesh.vertices[index + 2],
-                            1.0,
-                        );
-        
-                        x.mesh.vertices[index] = _position.x;
-                        index += 1;
-                        x.mesh.vertices[index] = _position.y;
-                        index += 1;
-                        x.mesh.vertices[index] = _position.z;
-                        index += 1;
-                    }
-                },
-                _ => {},
+        let mut index: usize = 0;
+        loop {
+            let mut _position: glm::Vec4 = glm::vec4(0.0, 0.0, 0.0, 0.01);
+            if index == item.mesh.vertices.len() {
+                break;
             }
+
+            _position = item.physics.as_ref().unwrap().transform * glm::vec4(
+                item.mesh.vertices[index],
+             item.mesh.vertices[index + 1],
+             item.mesh.vertices[index + 2],
+                1.0,
+            );
+
+            item.mesh.vertices[index] = _position.x;
+            index += 1;
+            item.mesh.vertices[index] = _position.y;
+            index += 1;
+            item.mesh.vertices[index] = _position.z;
+            index += 1;
         }
     }
 }
