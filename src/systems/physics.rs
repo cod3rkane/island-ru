b8203c9afe144ea00890ebb9b0292703713b3310
Cod3rkane <jcesar.developer@gmail.com>
2020-07-20 09:51:02 -0400

    spining by default

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -1,27 +1,29 @@
 use crate::core::game_state::GameState;
 extern crate nalgebra_glm as glm;
 
-pub fn physics_system(game_state: &mut GameState) {
+pub fn physics_system(game_state: &mut GameState, deltatime: f64) {
     for item in &mut game_state.entities {
         let mut index: usize = 0;
+        let mut _position: glm::Vec4 = glm::vec4(0.0, 0.0, 0.0, 0.0);
+        item.physics.as_mut().unwrap().rotate_z(0.01);
+
         loop {
-            let mut _position: glm::Vec4 = glm::vec4(0.0, 0.0, 0.0, 0.01);
             if index == item.mesh.vertices.len() {
                 break;
             }
 
             _position = item.physics.as_ref().unwrap().transform * glm::vec4(
                 item.mesh.vertices[index],
-             item.mesh.vertices[index + 1],
-             item.mesh.vertices[index + 2],
-                1.0,
+                item.mesh.vertices[index + 1],
+                item.mesh.vertices[index + 2],
+                0.01,
             );
 
-            item.mesh.vertices[index] = _position.x;
+            item.vertices[index] = _position.x;
             index += 1;
-            item.mesh.vertices[index] = _position.y;
+            item.vertices[index] = _position.y;
             index += 1;
-            item.mesh.vertices[index] = _position.z;
+            item.vertices[index] = _position.z;
             index += 1;
         }
     }
