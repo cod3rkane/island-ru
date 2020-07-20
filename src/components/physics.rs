fdc175e76569ae62e255d4dbf4b234ed46d35ccf
Cod3rkane <jcesar.developer@gmail.com>
2020-07-19 20:41:57 -0400

    Code improvements Physics component

--- src/components/physics.rs
+++ src/components/physics.rs
@@ -1,4 +1,4 @@
-use nalgebra_glm::{ Vec3, Mat4, mat4, translate, rotate_z };
+use nalgebra_glm::{ Vec3, Mat4, mat4, translate, rotate_z, scale };
 
 pub struct Physics {
     pub position: Vec3,
@@ -7,33 +7,30 @@ pub struct Physics {
 
 impl Physics {
     pub fn new(position: Vec3) -> Physics {
-        let mut matrix: Mat4 = mat4(
-            1.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            1.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            1.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            1.0,
+        let mut _transform: Mat4 = mat4(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0,
         );
-        matrix = translate(&mut matrix, &position);
+
+        _transform = translate(&mut _transform, &position);
 
         Physics {
             position,
-            transform: matrix,
+            transform: _transform,
         }
     }
 
     pub fn rotate_z(&mut self, angle: f32) {
         self.transform = rotate_z(&mut self.transform, angle);
+    }
+
+    pub fn translate(&mut self, position: Vec3) {
+        self.transform = translate(&mut self.transform, &position);
+    }
+
+    pub fn scale(&mut self, s: Vec3) {
+        self.transform = scale(&mut self.transform, &s);
     }
 }
