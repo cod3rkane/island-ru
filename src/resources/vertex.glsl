485db3e0ca1709bc9ce465037fb1b0dd4401bdab
Cod3rkane <jcesar.developer@gmail.com>
2020-07-19 20:41:33 -0400

    improves Vertex.glsl

--- src/resources/vertex.glsl
+++ src/resources/vertex.glsl
@@ -3,11 +3,9 @@
 layout (location = 0) in vec3 vertexPosition;
 layout (location = 1) in vec4 vertexColor;
 
-uniform mat4 transform;
-
 out vec4 color;
 
 void main() {
-  gl_Position = transform * vec4(vertexPosition, 1.0);
+  gl_Position = vec4(vertexPosition, 1.0);
   color = vertexColor;
 }
