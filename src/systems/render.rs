9bd5d367d49f5794e8b8e67279a83e8772f956e0
cod3rkane <jcesar.developer@gmail.com>
2020-07-25 22:14:29 -0400

    Clean space system

--- src/systems/render.rs
+++ src/systems/render.rs
@@ -18,33 +18,33 @@ pub fn render_system(game_state: &mut GameState) {
 
     for buffer in &mut game_state.buffers {
         buffer.bind();
-        
+
         buffer.vertices_vbo.bind();
         buffer.vertices_vbo.set_data(
             (game_state.buffer_data.vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
             game_state.buffer_data.vertices.as_ptr() as *const gl::types::GLvoid,
         );
         buffer.vertices_vbo.set_vertex_attr(0, 3, (3 * std::mem::size_of::<f32>()) as gl::types::GLsizei);
-    
+
         buffer.colors_vbo.bind();
         buffer.colors_vbo.set_data(
             (game_state.buffer_data.colors.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
             game_state.buffer_data.colors.as_ptr() as *const gl::types::GLvoid,
         );
         buffer.colors_vbo.set_vertex_attr(1, 4, (4 * std::mem::size_of::<f32>()) as gl::types::GLsizei);
-    
+
         buffer.indices_vbo.bind();
         buffer.indices_vbo.set_data(
             (game_state.buffer_data.indices.len() * std::mem::size_of::<gl::types::GLfloat>()) as gl::types::GLsizeiptr,
             game_state.buffer_data.indices.as_ptr() as *const gl::types::GLvoid,
         );
-    
+
         unsafe {
             gl::Enable(gl::BLEND);
             gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
-    
+
             gl::DrawElements(gl::TRIANGLES, game_state.buffer_data.indices.len() as i32, gl::UNSIGNED_INT, std::ptr::null());
-    
+
             gl::Disable(gl::BLEND);
         }
     }
