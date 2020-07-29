845d1340ac6a8250ec6344410ab20bed99a0913f
cod3rkane <jcesar.developer@gmail.com>
2020-07-28 23:06:30 -0400

    Entity code improvements

--- src/components/entity.rs
+++ src/components/entity.rs
@@ -1,11 +1,12 @@
-use crate::components::{ physics::Physics, mesh::Mesh };
-use nalgebra_glm::{ Vec3 };
+use crate::components::{ physics::Physics, mesh::Mesh, tile::Tile, tile::TileType };
+use nalgebra_glm::{ Vec3, vec3 };
 
 #[derive(Debug, PartialEq)]
 pub struct Entity {
     pub physics: Option<Physics>,
     pub mesh: Mesh,
     pub t_vertices: Vec<f32>,
+    pub tiles: Option<Vec<Tile>>,
 }
 
 impl Entity {
@@ -38,6 +39,41 @@ impl Entity {
                 0.5, 0.0, 0.0,
                 0.0, -0.5, 0.0,
             ],
+            tiles: None,
+        }
+    }
+
+    pub fn new_world(position: Vec3) -> Entity {
+        let _square = Mesh {
+            vertices: vec![
+                -0.5, 0.0, 0.0,
+                0.0, 0.5, 0.0,
+                0.5, 0.0, 0.0,
+                0.0, -0.5, 0.0,
+            ],
+            indices: vec![
+                0, 1, 2,
+                0, 2, 3,
+            ],
+            colors: vec![
+                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0,
+            ],
+        };
+        let _tile: Tile = Tile::new(TileType::GRASS, Physics::new(vec3(0.0, 0.0, 0.0)));
+
+        Entity {
+            physics: Some(Physics::new(position)),
+            mesh: _square,
+            t_vertices: vec![
+                -0.5, 0.0, 0.0,
+                0.0, 0.5, 0.0,
+                0.5, 0.0, 0.0,
+                0.0, -0.5, 0.0,
+            ],
+            tiles: Some(vec![_tile]),
         }
     }
 }
