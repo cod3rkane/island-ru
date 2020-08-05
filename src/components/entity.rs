b327a5f9b415442eebab0b91e28eb23ed5975dcb
cod3rkane <jcesar.developer@gmail.com>
2020-08-05 16:24:21 -0400

    clean Entity

--- src/components/entity.rs
+++ src/components/entity.rs
@@ -1,65 +1,44 @@
-use crate::components::{ physics::Physics, mesh::Mesh, tile::Tile, tile::TileType };
-use nalgebra_glm::{ Vec3, vec3, vec2 };
+use crate::components::{
+    mesh::Mesh, physics::Physics, texture::Texture, tile::Tile, tile::TileType,
+};
+use nalgebra_glm::{vec2, vec3, Vec3};
 
 #[derive(Debug, PartialEq)]
 pub struct Entity {
     pub physics: Option<Physics>,
     pub mesh: Mesh,
-    pub t_vertices: Vec<f32>,
     pub tiles: Option<Vec<Tile>>,
+    pub texture: Option<Texture>,
 }
 
 impl Entity {
     pub fn new_square(position: Vec3) -> Entity {
         let _triangle = Mesh {
-            vertices: vec![
-                -0.5, 0.0, 0.0,
-                0.0, 0.5, 0.0,
-                0.5, 0.0, 0.0,
-                0.0, -0.5, 0.0,
-            ],
-            indices: vec![
-                0, 1, 2,
-                0, 2, 3,
-            ],
+            vertices: vec![-0.5, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5, 0.0, 0.0, 0.0, -0.5, 0.0],
+            indices: vec![0, 1, 2, 0, 2, 3],
             colors: vec![
-                0.14902, 0.901961, 0.545098, 1.0,
-                0.14902, 0.901961, 0.545098, 1.0,
-                0.14902, 0.901961, 0.545098, 1.0,
-                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0, 0.14902, 0.901961, 0.545098, 1.0, 0.14902,
+                0.901961, 0.545098, 1.0, 0.14902, 0.901961, 0.545098, 1.0,
             ],
         };
 
         Entity {
             physics: Some(Physics::new(position)),
             mesh: _triangle,
-            t_vertices: vec![
-                -0.5, 0.0, 0.0,
-                0.0, 0.5, 0.0,
-                0.5, 0.0, 0.0,
-                0.0, -0.5, 0.0,
-            ],
             tiles: None,
+            texture: None,
         }
     }
 
     pub fn new_world(position: Vec3) -> Entity {
         let _square = Mesh {
             vertices: vec![
-                -1.0, 1.0, 0.0,
-                1.0, 1.0, 0.0,
-                -1.0, -1.0, 0.0,
-                1.0, -1.0, 0.0,
+                -1.0, 1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0,
             ],
-            indices: vec![
-                0, 1, 2,
-                1, 2, 3,
-            ],
+            indices: vec![0, 1, 2, 1, 2, 3],
             colors: vec![
-                0.14902, 0.901961, 0.545098, 1.0,
-                0.14902, 0.901961, 0.545098, 1.0,
-                0.14902, 0.901961, 0.545098, 1.0,
-                0.14902, 0.901961, 0.545098, 1.0,
+                0.14902, 0.901961, 0.545098, 1.0, 0.14902, 0.901961, 0.545098, 1.0, 0.14902,
+                0.901961, 0.545098, 1.0, 0.14902, 0.901961, 0.545098, 1.0,
             ],
         };
         let rows = 12;
@@ -72,24 +51,26 @@ impl Entity {
             for j in 0..columns {
                 let x = (j as f32) * tile_width;
                 let y = (i as f32) * tile_height;
-                let tile_type: TileType = if i == 0 { TileType::DIRT } else { TileType::GRASS };
-                tiles.push(Tile::new(tile_type, &mut Physics::new(vec3(x, y, 0.0)), vec2(i as f32, j as f32)));
+                let tile_type: TileType = if i == 0 {
+                    TileType::DIRT
+                } else {
+                    TileType::GRASS
+                };
+                tiles.push(Tile::new(
+                    tile_type,
+                    &mut Physics::new(vec3(x, y, 0.0)),
+                    vec2(i as f32, j as f32),
+                ));
             }
         }
         let mut world_physics = Physics::new(position);
         world_physics.rotate_z(-135.0);
 
-
         Entity {
             physics: Some(world_physics),
             mesh: _square,
-            t_vertices: vec![
-                -0.5, 0.0, 0.0,
-                0.0, 0.5, 0.0,
-                0.5, 0.0, 0.0,
-                0.0, -0.5, 0.0,
-            ],
             tiles: Some(tiles),
+            texture: None,
         }
     }
 }
