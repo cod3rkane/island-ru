64fdd806a994a0f0fa670f3cdd7ffe15b2f2f9b7
cod3rkane <jcesar.developer@gmail.com>
2020-08-15 20:59:31 -0400

    changes W A S D to arrow keys

--- src/systems/input.rs
+++ src/systems/input.rs
@@ -4,19 +4,19 @@ use nalgebra_glm::{ translate, vec3 };
 
 pub fn capture_input(_window: &mut Window, event: &WindowEvent, game_state: &mut GameState) {
     match event {
-        glfw::WindowEvent::Key(Key::W, _, Action::Press, _) => {
+        glfw::WindowEvent::Key(Key::Up, _, Action::Press, _) => {
             let t = translate(&mut game_state.view_matrix, &vec3(0.0, -0.2, 0.0));
             game_state.view_matrix = t;
         }
-        glfw::WindowEvent::Key(Key::S, _, Action::Press, _) => {
+        glfw::WindowEvent::Key(Key::Down, _, Action::Press, _) => {
             let t = translate(&mut game_state.view_matrix, &vec3(0.0, 0.2, 0.0));
             game_state.view_matrix = t;
         }
-        glfw::WindowEvent::Key(Key::A, _, Action::Press, _) => {
+        glfw::WindowEvent::Key(Key::Left, _, Action::Press, _) => {
             let t = translate(&mut game_state.view_matrix, &vec3(0.2, 0.0, 0.0));
             game_state.view_matrix = t;
         }
-        glfw::WindowEvent::Key(Key::D, _, Action::Press, _) => {
+        glfw::WindowEvent::Key(Key::Right, _, Action::Press, _) => {
             let t = translate(&mut game_state.view_matrix, &vec3(-0.2, 0.0, 0.0));
             game_state.view_matrix = t;
         }
