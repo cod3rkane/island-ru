e4f3cdc1e185660e5bb725e65455c5c3090080db
cod3rkane <jcesar.developer@gmail.com>
2020-08-16 15:51:56 -0400

    code improvements

--- src/core/game_state.rs
+++ src/core/game_state.rs
@@ -1,7 +1,7 @@
 use crate::components::{ entity::Entity, shader::Shader };
 use crate::core::{ buffers::*, shader::create_shader, texture::Texture };
 
-use nalgebra_glm::{ vec3, Mat4, mat4, translate, vec2, Vec2 };
+use nalgebra_glm::{ vec3, Vec3, Mat4, mat4, translate, vec2, Vec2 };
 extern crate image;
 use image::GenericImage;
 use image::GenericImageView;
@@ -21,7 +21,7 @@ pub struct GameState {
     pub world: Option<Entity>,
     pub textures: Option<Vec<Texture>>,
     pub mouse_pos: Vec2,
-    pub camera_zoom: f32,
+    pub camera_pos: Vec3,
 }
 
 pub fn initial_game_state() -> GameState {
@@ -40,8 +40,8 @@ pub fn initial_game_state() -> GameState {
         0.0, 0.0, 1.0, 0.0,
         0.0, 0.0, 0.0, 1.0,
     );
-    let camera_zoom = -8.0;
-    _view_matrix = translate(&mut _view_matrix, &vec3(0.0, 0.0, camera_zoom));
+    let camera_pos = vec3(0.0, 0.0, -8.0);
+    _view_matrix = translate(&mut _view_matrix, &camera_pos);
     let mut _projection_matrix: Mat4 = mat4(
         1.0, 0.0, 0.0, 0.0,
         0.0, 1.0, 0.0, 0.0,
@@ -66,6 +66,6 @@ pub fn initial_game_state() -> GameState {
         world: Some(Entity::new_world(vec3(0.0, 0.0, 0.0), &_texture)),
         textures: Some(vec![_texture]),
         mouse_pos: vec2(0.0, 0.0),
-        camera_zoom,
+        camera_pos,
     }
 }
