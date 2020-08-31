9b48d6c1e0cd556708d29aaca4164c75e3ed6226
Cod3rkane <jcesar.developer@gmail.com>
2020-08-31 15:32:24 -0400

    improves pathfinding

--- src/systems/interations.rs
+++ src/systems/interations.rs
@@ -1,8 +1,8 @@
 use crate::components::tile::*;
 use crate::core::game_state::GameState;
-use pathfinding::prelude::{absdiff, astar};
+use pathfinding::prelude::{absdiff, bfs};
 
-fn neighbors(node: &GridPos, neighborhood: &Vec<GridPos>) -> Vec<(GridPos, u32)> {
+fn neighbors(node: &GridPos, neighborhood: &Vec<GridPos>) -> Vec<GridPos> {
     let &GridPos(x, y) = node;
     let dirs: Vec<GridPos> = vec![
         GridPos(x + 1, y + 2),
@@ -14,17 +14,14 @@ fn neighbors(node: &GridPos, neighborhood: &Vec<GridPos>) -> Vec<(GridPos, u32)>
         GridPos(x - 2, y + 1),
         GridPos(x - 2, y - 1),
     ];
-    let mut result: Vec<(GridPos, u32)> = vec![];
+    let mut result: Vec<GridPos> = vec![];
 
     for dir in dirs {
         // 36x36 our MAP size @TODO use GRID_SIZE constant
         if dir.0 < 36 && dir.1 < 36 {
-            let weight: u32 = match neighborhood.iter().find(|n| **n == dir).is_some() {
-                true => 1,
-                _ => 99999
-            };
-
-            result.push((dir, weight));
+            if neighborhood.iter().find(|n| **n == dir).is_some() {
+                result.push(dir);
+            }
         }
     }
 
@@ -32,7 +29,7 @@ fn neighbors(node: &GridPos, neighborhood: &Vec<GridPos>) -> Vec<(GridPos, u32)>
 }
 
 pub fn interations_system(game_state: &mut GameState, delta_time: f32) {
-    static GOAL: GridPos = GridPos(6, 7);
+    static GOAL: GridPos = GridPos(6, 12);
     let all_nodes: Vec<GridPos> = game_state
         .world
         .as_ref()
@@ -45,11 +42,6 @@ pub fn interations_system(game_state: &mut GameState, delta_time: f32) {
         .map(|t| t.grid_pos)
         .collect();
 
-    let rs = astar(
-        &GridPos(7, 0),
-        |p| neighbors(p, &all_nodes),
-        |p| p.distance(&GOAL) / 3,
-        |p| *p == GOAL,
-    );
-    println!("here {:?}", rs);
+    let rs = bfs(&GridPos(7, 0), |p| neighbors(p, &all_nodes), |p| *p == GOAL);
+    //println!("here {:?}", rs);
 }
